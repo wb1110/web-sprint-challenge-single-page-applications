950f75cd90e6ec829e46f4c21bca5a931c7c8e99
wb1110 <strengthcoachwb@gmail.com>
2021-11-12 13:32:47 -0600

    slowly making progress

--- src/App.js
+++ src/App.js
@@ -1,47 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Link  } from "react-router-dom";
 import Form from "./Form";
 import Pizza from "./Assets/Pizza.jpg";
 import styled from "styled-components";
 import "./index.css";
+import axios from "axios";
+
+const Nav = styled.nav`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+padding: 0 2%;
+`
+const List = styled.ul`
+display: flex;
+`
+const NavLink = styled.li`
+text-decoration: none;
+list-style-type: none;
+border: solid black;
+padding: 0 2%;
+width: 20vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+const Background = styled.div`
+  background-image: url(${Pizza});
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 50vh;
+`
+const initialFormValues = {
+  username: "",
+  size: "",
+  pepperoni: false,
+  sausage: false,
+  canadianBacon: false,
+  spicyItalian: false,
+  grilledChicken: false,
+  onions: false,
+  greenPepper: false,
+  dicedTomatos: false,
+  blackOlives: false,
+  roastedGarlic: false,
+  artichokeHearts: false,
+  threeCheese: false,
+  pineapple: false,
+  extraCheese: false,
+  special: ""
+}
 
 function App () {
+  const [formValues, setFormValues] = useState(initialFormValues);
 
-  const initialForm = {
-    name: "",
-    size: "",
-    sauce: "",
-    substitute: "",
-    special: ""
+  const handleSubmit = () => {
+    axios.post('https://reqres.in/api/orders', formValues)
+    .then(res => {
+      setFormValues(res.data)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  const handleChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value});
   }
 
-  const Nav = styled.nav`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 0 2%;
-  `
-  const List = styled.ul`
-    display: flex;
-  `
-  const NavLink = styled.li`
-    text-decoration: none;
-    list-style-type: none;
-    border: solid black;
-    padding: 0 2%;
-    width: 20vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `
-  const Background = styled.div`
-      background-image: url(${Pizza});
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      height: 50vh;
-  `
+
+
+
 
   return (
     <div className='main-container'>
@@ -60,7 +90,9 @@ function App () {
           <h1>Your favorite food, delivered while coding</h1>
           <Link id="order-pizza" to="/pizza" >Pizza?</Link>
       </Background>
-      <Route path="/pizza" component={Form}/>
+      <Route path="/pizza">
+        <Form formValues={formValues} change={handleChange} submit={handleSubmit}/>
+      </Route>
       <Route exact path="/"/>
  
     </div>
--- src/Form.js
+++ src/Form.js
@@ -1,70 +1,91 @@
 import React from "react";
 import styled from "styled-components";
 
-const Form = () => {
-
 const Container = styled.div`
     width: 80%;
     display: flex;
     justify-content: center;
 `
-const Form = styled.form`
+const FormStyle = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
     border: solid black;
 `
+
+const Form = (props) => {
+    const {   change, submit    } = props;
+    const {username, checked, special} = props.formValues;
+
+    const onChange = (e) => {
+        const { name, value, checked, type   } = e.target;
+        const newVal = type === 'checkbox' ? checked : value;
+        change(name, newVal);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        submit();
+    }
+
   return (
     <Container>
-    <Form id="pizza-form">
+    <FormStyle id="pizza-form" onSubmit={onSubmit}>
         <label>
             Name
-            <input id="name-input"/>
+            <input id="name-input"
+                type="text"
+                name="username"
+                value={username}
+                onChange={onChange}
+            />
         </label>
         <label>
             Choice of Size
             <select id="size-dropdown">
-                <option value="null">Select a size</option>
-                <option value="small">Small</option>
-                <option value="medium">Medium</option>
-                <option value="large">Large</option>
+                <option onChange={onChange} checked={checked}value="null">Select a size</option>
+                <option onChange={onChange} checked={checked}value="small">Small</option>
+                <option onChange={onChange} checked={checked}value="medium">Medium</option>
+                <option onChange={onChange} checked={checked}value="large">Large</option>
             </select>
         </label>
+        <div>
         <label>
             Choice of Sauce
-            <input type="radio" value="originalRed" name="originalRed"/>Original Red
-            <input type="radio" value="garlicRanch" name="garlicRanch"/>Garlic Ranch
-            <input type="radio" value="bbqSauce" name="bbqSauce"/>BBQ Sauce
-            <input type="radio" value="spinachAlfredo" name="spinachAlfredo"/>Spinach Alfredo
+            <input type="radio" checked={checked} onChange={onChange} value="originalRed" name="sauce"/>Original Red
+            <input type="radio" checked={checked} onChange={onChange} value="garlicRanch" name="sauce"/>Garlic Ranch
+            <input type="radio" checked={checked} onChange={onChange} value="bbqSauce" name="sauce"/>BBQ Sauce
+            <input type="radio" checked={checked} onChange={onChange} value="spinachAlfredo" name="sauce"/>Spinach Alfredo
         </label>
+        </div>
         <label>
             Add Toppings
-            <input type="checkbox" name="pepperoni" value="pepperoni"/>Pepperoni
-            <input type="checkbox" name="sausage" value="sausage"/>Sausage
-            <input type="checkbox" name="canadianBacon" value="canadianBacon"/>Canadian Bacon
-            <input type="checkbox" name="spicyItalian" value="spicyItalian"/>Spicy Italian Sausage
-            <input type="checkbox" name="grilledChicken" value="grilledChicken"/>Grilled Chicken
-            <input type="checkbox" name="onions" value="onions"/>Onions
-            <input type="checkbox" name="greenPepper" value="greenPepper"/>Green Pepper
-            <input type="checkbox" name="dicedTomatos" value="dicedTomatos"/>Diced Tomatos
-            <input type="checkbox" name="blackOlives" value="blackOlives"/>Black Olives
-            <input type="checkbox" name="roastedGarlic" value="roastedGarlic"/>Roasted Garlic
-            <input type="checkbox" name="artichokeHearts" value="artichokeHearts"/>Artichoke Hearts
-            <input type="checkbox" name="threeCheese" value="threeCheese"/>Three Cheese
-            <input type="checkbox" name="pineapple" value="pineapple"/>Pineapple
-            <input type="checkbox" name="extraCheese" value="extraCheese"/>Extra Cheese
+            <input type="checkbox" name="pepperoni" checked={checked} onChange={onChange}/>Pepperoni
+            <input type="checkbox" name="sausage" checked={checked} onChange={onChange}/>Sausage
+            <input type="checkbox" name="canadianBacon" checked={checked} onChange={onChange}/>Canadian Bacon
+            <input type="checkbox" name="spicyItalian" checked={checked} onChange={onChange}/>Spicy Italian Sausage
+            <input type="checkbox" name="grilledChicken" checked={checked} onChange={onChange}/>Grilled Chicken
+            <input type="checkbox" name="onions" checked={checked} onChange={onChange}/>Onions
+            <input type="checkbox" name="greenPepper" checked={checked} onChange={onChange}/>Green Pepper
+            <input type="checkbox" name="dicedTomatos" checked={checked} onChange={onChange}/>Diced Tomatos
+            <input type="checkbox" name="blackOlives" checked={checked} onChange={onChange}/>Black Olives
+            <input type="checkbox" name="roastedGarlic" checked={checked} onChange={onChange}/>Roasted Garlic
+            <input type="checkbox" name="artichokeHearts" checked={checked} onChange={onChange}/>Artichoke Hearts
+            <input type="checkbox" name="threeCheese" checked={checked} onChange={onChange}/>Three Cheese
+            <input type="checkbox" name="pineapple" checked={checked} onChange={onChange}/>Pineapple
+            <input type="checkbox" name="extraCheese" checked={checked} onChange={onChange}/>Extra Cheese
         </label>
         <label>
             Choice of Substitute
-            <input type="checkbox" name="glutenFree" value="glutenFree"/>Gluten Free Crust (+$1.00)
+            <input type="checkbox" name="glutenFree" checked={checked} onChange={onChange}/>Gluten Free Crust (+$1.00)
         </label>
         <label>
             Special Instructions
-            <input id="special-text" type="text"/>
+            <input id="special-text" type="text" name="special" value={special} onChange={onChange}/>
         </label>
-        <input id="order-button" type='submit'/>
+        <button id="order-button" type='submit'>Add to Order</button>
 
-    </Form>
+    </FormStyle>
     </Container>
   );
 };
